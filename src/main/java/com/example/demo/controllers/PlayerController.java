fc0943baaf2212693664da77a76c071e4d4eca7c
Cristian Cabarcas <cridejecato@gmail.com>
2021-12-15 03:14:43 -0500

    Operaciones con limite de resultados

--- src/main/java/com/example/demo/controllers/PlayerController.java
+++ src/main/java/com/example/demo/controllers/PlayerController.java
@@ -4,10 +4,13 @@ import com.example.demo.models.PlayerModel;
 import com.example.demo.services.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping("/api/players")
 public class PlayerController {
 
     @Autowired
@@ -16,5 +19,20 @@ public class PlayerController {
     @GetMapping
     public Flux<PlayerModel> getAll(){
         return playerService.getAll();
+    }
+
+    @GetMapping("/age/34")
+    public Flux<PlayerModel> getByAge(){
+        return playerService.getByAge(34);
+    }
+
+    @GetMapping("/club/{name}")
+    public Flux<PlayerModel> getByClub(@PathVariable("name") String club){
+        return playerService.getByClub(club);
+    }
+
+    @GetMapping("/rancking/{nationality}")
+    public Flux<PlayerModel> getRanckingByNationality(@PathVariable("nationality") String nationality){
+        return playerService.getRanckingByNationality(nationality);
     }
 }
--- src/main/java/com/example/demo/services/PlayerService.java
+++ src/main/java/com/example/demo/services/PlayerService.java
@@ -1,12 +1,14 @@
 package com.example.demo.services;
 
-import com.example.demo.CsvUtilFile;
-import com.example.demo.Player;
 import com.example.demo.models.PlayerModel;
 import com.example.demo.repositories.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Comparator;
+import java.util.Objects;
 
 @Service
 public class PlayerService {
@@ -15,6 +17,33 @@ public class PlayerService {
     private PlayerRepository playerRepository;
 
     public Flux<PlayerModel> getAll(){
-        return playerRepository.findAll().limitRequest(10);
+        return playerRepository.findAll().limitRequest(1000);
+    }
+
+    public Flux<PlayerModel> getByAge(int age) {
+        return playerRepository.findAll()
+                .limitRequest(1000)
+                .buffer(100)
+                .flatMap(player -> Flux.fromStream(player.parallelStream()))
+                .filter(player -> player.getAge() > age);
+
+    }
+
+    public Flux<PlayerModel> getByClub(String club) {
+        return playerRepository.findAll()
+                .limitRequest(1000)
+                .buffer(100)
+                .flatMap(player -> Flux.fromStream(player.parallelStream()))
+                .filter(playerNoNullClub -> Objects.nonNull(playerNoNullClub.getClub()))
+                .filter(player -> player.getClub().equals(club));
+    }
+
+   public Flux<PlayerModel> getRanckingByNationality(String nationality) {
+        return playerRepository.findAll()
+                .limitRequest(1000)
+                .buffer(100)
+                .flatMap(player -> Flux.fromStream(player.parallelStream()))
+                .filter(player -> player.getNational().equals(nationality))
+                .sort(Comparator.comparingDouble(PlayerModel::getWinners).reversed());
     }
 }
